4456769884b93275ebc2d2f96c3d12325c177605
chenxiaoqing <chenxiaoqing@itsmycar.cn>
2020-03-24 18:08:17 +0800

    修改木块名称

--- src/main/java/cn/worth/sys/enums/EntityTypeEnum.java
+++ src/main/java/cn/worth/sys/enums/EntityTypeEnum.java
@@ -1,35 +1,23 @@
 package cn.worth.sys.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author myireland
  * @version 1.0.0
  * @date 2019-08-06
  * @description
  **/
+@Getter
+@AllArgsConstructor
 public enum EntityTypeEnum {
 
-    COMMON(0, "普通类型"), ADMIN(1, "管理员类型"), WX(2, "微信类型");
+    COMMON(0, "普通类型"),
+    ADMIN(1, "管理员类型"),
+    WX(2, "微信类型")
+    ;
+
     private int code;
     private String desc;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    EntityTypeEnum(int code, String desc) {
-        this.code = code;
-        this.desc = desc;
-    }
 }
