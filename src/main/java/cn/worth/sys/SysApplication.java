01a68e9afacf755dffd34ceaec96990d187cf5de
chenxiaoqing <chenxiaoqing@qianxx.com>
2020-03-27 18:00:13 +0800

    添加扫描目录

--- src/main/java/cn/worth/sys/SysApplication.java
+++ src/main/java/cn/worth/sys/SysApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"cn.worth.sys.*", "cn.worth.oauth2.*"})
 @MapperScan("cn.worth.**.mapper")
 @SpringBootApplication
 public class SysApplication {
